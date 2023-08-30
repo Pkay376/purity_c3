62df46df0626d4733bbacc99a97ad53015aaf270
Pkay376 <puritykidiavai@gmail.com>
2023-08-30 23:36:18 +0300

    First commit

---
+++ purity_c3/src/App.js
@@ -0,0 +1,68 @@
+// import logo from './logo.svg';
+
+import './App.css';
+
+import Button from './components/Button';
+
+ 
+
+function App() {
+
+  return (
+
+    <div className="App">
+
+      <h1>Purity Kidiavai</h1>
+
+      <h2>Educational Background</h2>
+
+      <p>Certificate in Frontend Development</p>
+
+      <p>Azubi Africa</p>
+
+     
+
+      <h2>Professional Background</h2>
+
+      <p>Frontend Developer</p>
+
+      <p>Sanlam Investment</p>
+
+     
+
+      <h2>Skills</h2>
+
+      <ul>
+
+        <li>JavaScript</li>
+
+        <li>HTML/CSS</li>
+        
+        <li>React</li>
+
+
+      </ul>
+
+ 
+
+      <Button label="Log in"/>
+
+      <Button label="Sign in"/>
+
+ 
+
+     
+
+ 
+
+    </div>
+
+ 
+
+  );
+
+}
+
+ 
+
+export default App;
